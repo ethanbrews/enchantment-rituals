06fa262098800676f88ba0a4110b90e2ced13b66
Ethan Brews <ethan@ethanbrews.me>
2026-02-11 12:54:12 +0000

    init

---
+++ common/src/main/java/me/ethanbrews/ExampleMod.java
@@ -0,0 +1,9 @@
+package me.ethanbrews;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "enchantmentrituals";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
